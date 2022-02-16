0b052b560f0e5262ed4fc8f2c4368a2d3a4085ad
Andrew-Michaelis <amichaelis70@s.tooeletech.edu>
2022-02-16 15:27:18 -0700

    first commit

---
+++ search_page.php
@@ -0,0 +1,21 @@
+<?php include "includes/header.php"; ?>
+
+<!-- Navigation -->
+<?php include "includes/navigation.php"; ?>
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- Blog search Column -->
+        <?php include "includes/search.php"; ?>
+
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/sidebar.php"; ?>
+
+    </div>
+
+    <hr>
+
+<?php include "includes/footer.php"; ?>
